e3e0fdbfc3ecd7a062ad430974c0a083a2cb845a
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-04-15 18:00:19 -0400

    port

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
 var db = require('./config/db');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 
 require('./app/routes')(app);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
 
-exports = module.exports = app; 
+exports = module.exports = app;

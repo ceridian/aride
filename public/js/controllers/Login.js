1145ff41ff4442654810c8ea87d03e0d23b60b04
Jacob Van Vleet <jacob.vanvleet@gmail.com>
2015-04-15 23:06:28 -0400

    test

--- public/js/controllers/Login.js
+++ public/js/controllers/Login.js
@@ -1,3 +1,10 @@
 angular.module('Login', []).controller('LoginSetup', function($scope){
-
+  this.login = function(main){
+    console.log(email, pass);
+		$http.post('/login', {email: email, pass: pass}).success(function(data, status, headers, config){
+			console.log(data, status);
+    }).error(function(data, status, headers, config) {
+			console.log(data.status);
+		});
+  }
 });
